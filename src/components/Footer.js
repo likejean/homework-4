d6cf939216be5ac046a008631f1027526526c569
likejean <popachs@yahoo.com>
2020-04-04 17:21:22 -0700

    General Project Cleanup

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ export default () => {
     return (
         <div style={{ textAlign: 'center' }}>
             <hr></hr>
-            <span style={{ fontSize: 45, color: '#26a69a' }}>Phone Number: 1-800-101-4444</span>
+            <span style={{ fontSize: 45, color: '#26a69a' }}>Created by Sergey Popach</span>
         </div>
     )
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Range from './Range';
 import Counter from './Counter';
+import Validation from '../helpers/Validation';
 
 
 export default ({ name }) => {
@@ -16,8 +17,6 @@ export default ({ name }) => {
                 lower: false
             }
         });
-    const [ buttonMap, setButtonMap ] = useState({lower: [-1], upper: [1]});
-    const [ error, setError ] = useState('');
 
     const handleRangeChange = e => {
         const { name, value } = e.target;
@@ -26,68 +25,11 @@ export default ({ name }) => {
         setRangeLimits({ ...rangeLimits, [ name ]: value });
         let tempUpper = rangeLimits[ "upper" ];
         let tempLower = rangeLimits[ "lower" ];
-        validation (name, value, fieldStatus, tempUpper, tempLower, errors);
-
-        let range = parseInt(rangeLimits.upper) - parseInt(rangeLimits.lower);
-        let arrRight = [];
-        let arrLeft = [];
-        for (let i = parseInt(rangeLimits.lower); i <= parseInt(rangeLimits.lower) + range; i++){
-            arrRight.push(i);
-            arrLeft.push(-(parseInt(rangeLimits.lower) + parseInt(rangeLimits.lower) + range - i));
-        }
-        range >= 0 ?
-            setButtonMap({lower: arrLeft, upper: arrRight})
-            :
-            setError('Invalid Inputs! Check if Upper Limit is Greater than Lower Limit');
-
-        //User Error Validation
+        Validation (name, value, fieldStatus, tempUpper, tempLower, errors);
     }
-
-    const validation =  (name, value, fieldStatus, tempUpper, tempLower, errors) => {
-        switch (name) {
-            case 'upper':
-                errors.upper =
-                    isNaN(value) || Number.isInteger(value)
-                        ? 'ERROR! Input must be an INTEGER'
-                        : parseInt(value) >= 10
-                        ? 'ERROR! Must be less than 10'
-                        : parseInt(value) < tempLower
-                            ? 'ERROR! Upper Limit Must be GREATER than Lower Limit'
-                            : ''
-                ;
-                fieldStatus.lower =
-                    parseInt(value) < tempLower
-                        ? true
-                        : false
-                ;
-
-                break;
-            case 'lower':
-                errors.lower =
-                    isNaN(value) || Number.isInteger(value)
-                        ? 'ERROR! Input must be an INTEGER'
-                        : parseInt(value) < 1
-                        ? 'ERROR! Must be greater than zero'
-                        : parseInt(value) > tempUpper
-                            ? 'ERROR! Lower Limit Must be Less than Upper Limit'
-                            : ''
-                ;
-                fieldStatus.upper =
-                    parseInt(value) > tempUpper
-                        ? true
-                        : false
-                ;
-                break;
-            default:
-                break;
-        }
-    }
-
 
     const props = {
         rangeLimits: rangeLimits,
-        buttonMap: buttonMap,
-        error: error,
         handleRangeChange: handleRangeChange
     }
     return (
@@ -96,7 +38,6 @@ export default ({ name }) => {
             <hr></hr>
             <Range {...props}/>
             <Counter {...props}/>
-
         </React.Fragment>
     )
 }
